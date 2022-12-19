1e50463b47ac532881fd5c492ff63aba01882045
anivcs <111776229+anivcs@users.noreply.github.com>
2022-12-18 21:14:53 -0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,41 +1,113 @@
 
-     function displayAnswer1() {
-      if (document.getElementById('option-11').checked) {
-        document.getElementById('block-11').style.border = '3px solid limegreen'
-        document.getElementById('result-11').style.color = 'limegreen'
-        document.getElementById('result-11').innerHTML = 'Correct!'
-      }
-      if (document.getElementById('option-12').checked) {
-        document.getElementById('block-12').style.border = '3px solid red'
-        document.getElementById('result-12').style.color = 'red'
-        document.getElementById('result-12').innerHTML = 'Incorrect!'
-        showCorrectAnswer1()
-      }
-      if (document.getElementById('option-13').checked) {
-        document.getElementById('block-13').style.border = '3px solid red'
-        document.getElementById('result-13').style.color = 'red'
-        document.getElementById('result-13').innerHTML = 'Incorrect!'
-        showCorrectAnswer1()
-      }
-      if (document.getElementById('option-14').checked) {
-        document.getElementById('block-14').style.border = '3px solid red'
-        document.getElementById('result-14').style.color = 'red'
-        document.getElementById('result-14').innerHTML = 'Incorrect!'
-        showCorrectAnswer1()
-      }
-    }
-  
-    function showCorrectAnswer1() {
-      let showAnswer1 = document.createElement('p')
-      showAnswer1.innerHTML = 'Show Corrent Answer'
-      showAnswer1.style.position = 'relative'
-      showAnswer1.style.top = '-180px'
-      showAnswer1.style.fontSize = '1.75rem'
-      document.getElementById('showanswer1').appendChild(showAnswer1)
-      showAnswer1.addEventListener('click', () => {
-        document.getElementById('block-11').style.border = '3px solid limegreen'
-        document.getElementById('result-11').style.color = 'limegreen'
-        document.getElementById('result-11').innerHTML = 'Correct!'
-        document.getElementById('showanswer1').removeChild(showAnswer1)
-      })
-    }
+function displayAnswer1() {
+  if (document.getElementById('option-1').checked) {
+    document.getElementById('block-1').style.border = '3px solid limegreen'
+    document.getElementById('result-1').style.color = 'limegreen'
+    document.getElementById('result-1').innerHTML = 'Correct!'
+  }
+  if (document.getElementById('option-2').checked) {
+    document.getElementById('block-2').style.border = '3px solid red'
+    document.getElementById('result-2').style.color = 'red'
+    document.getElementById('result-2').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-3').checked) {
+    document.getElementById('block-3').style.border = '3px solid red'
+    document.getElementById('result-3').style.color = 'red'
+    document.getElementById('result-3').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-4').checked) {
+    document.getElementById('block-4').style.border = '3px solid red'
+    document.getElementById('result-4').style.color = 'red'
+    document.getElementById('result-4').innerHTML = 'Incorrect!'
+  }
+}
+
+
+function displayAnswer2() {
+  if (document.getElementById('option-5').checked) {
+    document.getElementById('block-5').style.border = '3px solid red'
+    document.getElementById('result-5').style.color = 'red'
+    document.getElementById('result-5').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-6').checked) {
+    document.getElementById('block-6').style.border = '3px solid limegreen'
+    document.getElementById('result-6').style.color = 'limegreen'
+    document.getElementById('result-6').innerHTML = 'Correct!'
+  }
+  if (document.getElementById('option-7').checked) {
+    document.getElementById('block-7').style.border = '3px solid red'
+    document.getElementById('result-7').style.color = 'red'
+    document.getElementById('result-7').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-8').checked) {
+    document.getElementById('block-8').style.border = '3px solid red'
+    document.getElementById('result-8').style.color = 'red'
+    document.getElementById('result-8').innerHTML = 'Incorrect!'
+  }
+}
+function displayAnswer3() {
+  if (document.getElementById('option-9').checked) {
+    document.getElementById('block-9').style.border = '3px solid red'
+    document.getElementById('result-9').style.color = 'red'
+    document.getElementById('result-9').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-10').checked) {
+    document.getElementById('block-10').style.border = '3px solid limegreen'
+    document.getElementById('result-10').style.color = 'limegreen'
+    document.getElementById('result-10').innerHTML = 'Correct!'
+  }
+  if (document.getElementById('option-11').checked) {
+    document.getElementById('block-11').style.border = '3px solid red'
+    document.getElementById('result-11').style.color = 'red'
+    document.getElementById('result-11').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-12').checked) {
+    document.getElementById('block-12').style.border = '3px solid red'
+    document.getElementById('result-12').style.color = 'red'
+    document.getElementById('result-12').innerHTML = 'Incorrect!'
+  }
+}
+function displayAnswer4() {
+  if (document.getElementById('option-13').checked) {
+    document.getElementById('block-13').style.border = '3px solid red'
+    document.getElementById('result-13').style.color = 'red'
+    document.getElementById('result-13').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-14').checked) {
+    document.getElementById('block-14').style.border = '3px solid red'
+    document.getElementById('result-14').style.color = 'red'
+    document.getElementById('result-14').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-15').checked) {
+    document.getElementById('block-15').style.border = '3px solid limegreen'
+    document.getElementById('result-15').style.color = 'limegreen'
+    document.getElementById('result-15').innerHTML = 'Correct!'
+  }
+  if (document.getElementById('option-16').checked) {
+    document.getElementById('block-16').style.border = '3px solid red'
+    document.getElementById('result-16').style.color = 'red'
+    document.getElementById('result-16').innerHTML = 'Incorrect!'
+  }
+}
+function displayAnswer5() {
+  if (document.getElementById('option-17').checked) {
+    document.getElementById('block-17').style.border = '3px solid red'
+    document.getElementById('result-17').style.color = 'red'
+    document.getElementById('result-17').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-18').checked) {
+    document.getElementById('block-18').style.border = '3px solid red'
+    document.getElementById('result-18').style.color = 'red'
+    document.getElementById('result-18').innerHTML = 'Incorrect!'
+  }
+  if (document.getElementById('option-19').checked) {
+    document.getElementById('block-19').style.border = '3px solid limegreen'
+    document.getElementById('result-19').style.color = 'limegreen'
+    document.getElementById('result-19').innerHTML = 'Correct!'
+  }
+  if (document.getElementById('option-20').checked) {
+    document.getElementById('block-20').style.border = '3px solid red'
+    document.getElementById('result-20').style.color = 'red'
+    document.getElementById('result-20').innerHTML = 'Incorrect!'
+  }
+}

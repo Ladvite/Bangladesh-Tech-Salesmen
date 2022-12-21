fcc21af06584893ae0b8a751c0e9b2550d9ee682
SidSun13 <111776145+SidSun13@users.noreply.github.com>
2022-12-20 22:44:35 -0800

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,12 +1,12 @@
 <?php
-
+//initiates score and the different values from the html file
 $q_score = $_POST['quality'];
 $feedback_txt = $_POST['suggestion'];
 $conn = mysqli_connect("localhost", "root","", "practice");
-$query ="insert into feedback(quality_score, feedback)values($q_score, '$feedback_txt')";
-$result = mysqli_query($conn, $query);
-if($result)
-  echo 'Thank you for your feedback. We\'ll appreciate!';
+$query ="insert into feedback(quality_score, feedback)values($q_score, '$feedback_txt')"; //queries the database for the values of score and the input of feedback_txt
+$result = mysqli_query($conn, $query); 
+if($result)//echoes text for result approved or unable to follow through
+  echo 'Thank you for your feedback.';
 else
 die("Something terrible happened. Please try again. ");
 

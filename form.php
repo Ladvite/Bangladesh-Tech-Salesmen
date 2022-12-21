fdf9d6726fbfb06d0e5913e9659a51e705708ed5
SidSun13 <111776145+SidSun13@users.noreply.github.com>
2022-12-20 22:47:50 -0800

    Update form.php

--- form.php
+++ form.php
@@ -1,18 +1,18 @@
 <?php
 if (isset($_POST['submit'])) {
     if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']) {
-        
+        //instantiates username, password, and email
         $username = $_POST['username'];
         $password = $_POST['password'];
         $email = $_POST['email'];
-
+        //talks to the server to connect with the hostname being part of the local machine
         $host = "localhost";
         $dbUsername = "root";
         $dbPassword = "";
         $dbName = "test";
-
+//attempts a connection to the mysql databse
         $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-
+//error if unable to connect, and if able to, move to grant access to input
         if ($conn->connect_error) {
             die('Could not connect to the database.');
         }
@@ -30,7 +30,7 @@ if (isset($_POST['submit'])) {
 
             if ($rnum == 0) {
                 $stmt->close();
-
+//requirements for input
                 $stmt = $conn->prepare($Insert);
                 $stmt->bind_param("ssssii",$username, $password, $email);
                 if ($stmt->execute()) {
